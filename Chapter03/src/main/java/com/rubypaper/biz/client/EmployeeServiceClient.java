fbcf7ecba5e1f763bf614f08a4e4f02e05437150
Kimdonghyun <thisisnew@naver.com>
2020-09-16 22:04:15 +0900

    jpa 영속성 공부

--- Chapter03/src/main/java/com/rubypaper/biz/client/EmployeeServiceClient.java
+++ Chapter03/src/main/java/com/rubypaper/biz/client/EmployeeServiceClient.java
@@ -17,14 +17,16 @@ public class EmployeeServiceClient {
 		EntityTransaction tx = em.getTransaction();
 		
 		try {
-			Employee employee = new Employee();
-			employee.setName("둘리");
+//			Employee employee = new Employee();
+//			employee.setName("둘리");
+//			
+//			tx.begin();
+//			
+//			em.persist(employee);
+//			tx.commit();
 			
-			tx.begin();
-			
-			em.persist(employee);
-			
-			tx.begin();
+			Employee findEmp = em.find(Employee.class, 1L);
+			System.out.println("검색된 직원 정보:" + findEmp.toString());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
